0f2f79a108674e77cb4f07733282469675999894
izhar9 <112800181+izhar9@users.noreply.github.com>
2024-02-24 14:44:54 +0530

    Update Layout.jsx

--- 07reactRouter/src/Layout.jsx
+++ 07reactRouter/src/Layout.jsx
@@ -10,7 +10,8 @@ function Layout() {
     <Outlet />   {/* Outlet yaha bhi use karenge waha ka component change hota rahega 
                     ex. Header and Footer same rahega aur Outlet ke jagah par dusra component change kar sakte hai(jaise Header aur footer same
                     chahiye aur bich me home ya about us page  ya koi dusra aur page daal sakte hai.) 
-                    Outlet use karne par hume index file(is page ke liye hum main.jsx ka use kar rahe hai) me jaa kar batana padega ki hum ek Layout use kar rahe hai aur uske ander baki sab render kar rahe hai */}
+                    Outlet use karne par hume index file(is page ke liye hum main.jsx ka use kar rahe hai) me jaa kar batana padega ki hum ek Layout use kar rahe hai aur uske ander baki sab render kar rahe hai 
+                    isme ractRouter DOm ki taraf se nesting ki jaa sakte hai.*/}
     <Footer />
     </>
   )
