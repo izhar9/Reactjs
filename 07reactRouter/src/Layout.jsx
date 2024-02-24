8f3d01fac2db9a47962591f004dbe50fad265582
izhar9 <112800181+izhar9@users.noreply.github.com>
2024-02-24 09:11:50 +0000

    ReactRouter

---
+++ 07reactRouter/src/Layout.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import { Outlet } from 'react-router-dom'
+
+function Layout() {
+  return (
+    <>
+    <Header/>
+    <Outlet />   {/* Outlet yaha bhi use karenge waha ka component change hota rahega 
+                    ex. Header and Footer same rahega aur Outlet ke jagah par dusra component change kar sakte hai(jaise Header aur footer same
+                    chahiye aur bich me home ya about us page  ya koi dusra aur page daal sakte hai.) 
+                    Outlet use karne par hume index file(is page ke liye hum main.jsx ka use kar rahe hai) me jaa kar batana padega ki hum ek Layout use kar rahe hai aur uske ander baki sab render kar rahe hai */}
+    <Footer />
+    </>
+  )
+}
+
+export default Layout
---
+++ 07reactRouter/src/main.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
+import Github, { githubInfoLoader } from './components/Github/Github.jsx'
+
+
+// 1st way of define routing
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout/>,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />
+//       },
+//       {
+//         path: "about",
+//         element: <About />
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />
+//       }
+//     ]
+//   }
+// ])
+
+
+// 2nd way of define routing
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:userid' element={<User />} />
+      <Route 
+      loader={githubInfoLoader}
+      path='github' 
+      element={<Github />}
+       />
+    </Route>
+  )
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />    {/* RouterProvider hamesha ek prop leta hai yaha router props diye hai jise upper define kiye hai */}
+  </React.StrictMode>,
+)
